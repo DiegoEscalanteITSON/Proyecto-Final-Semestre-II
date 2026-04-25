ec94a26728b52cbab69417cd67cd40f9561cc874
DiegoEscalanteITSON <diego.escalante272259@potros.itson.edu.mx>
2026-04-25 14:46:06 -0700

    Imagenes

--- src/Modelos/Usuario.java
+++ src/Modelos/Usuario.java
@@ -14,5 +14,67 @@ public class Usuario {
     private String apellido;
     private String correo;
     private String password;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
+    public Usuario (String nombre, String apellido, String correo, String password) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.password = password;
+    }
 }
